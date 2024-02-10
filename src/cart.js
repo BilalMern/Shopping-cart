c4211bf93aaa21d423efae0aa0b874f08065eace
BilalMern <bilal.mern@gmail.com>
2024-02-10 20:51:11 +0500

    cart updates

--- src/cart.js
+++ src/cart.js
@@ -1,6 +1,25 @@
-let basket = JSON.parse(localStorage.getItem("data")) || [];  
-let calculation = ()=>{
-    let cartIcon =document.getElementById("cartAmount")
-    cartIcon.innerHTML = basket.map((x)=> x.item).reduce((x,y)=>x+y,0);
-  }
-  calculation()
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+calculation();
+
+//! BUILDING CART ITEMS:
+let generateCartItems = ()=>{
+if (basket.length !== 0){
+} else{
+    shoppingCart.innerHTML =``
+    label.innerHTML =`
+    <h2>Cart Is Empty</h2>
+    <a href = "index.html"> 
+    <button class ="HomeBtn">Back To Home</button>
+    </a>
+    `
+}
+}
+generateCartItems()
